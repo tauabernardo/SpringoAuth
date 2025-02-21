1f7aaad279b4faf34e73a1b3e145501d343e2fed
Tauã Bernardo <tbptffc@gmail.com>
2025-02-21 18:36:52 -0300

    Exclusão do dotEnvConfig

--- src/main/java/com/example/spring/controller/HomeController.java
+++ src/main/java/com/example/spring/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+
+
     @GetMapping("/")
     public String home(@AuthenticationPrincipal Object principal, Model model) {
         // Verifica se o usuário está autenticado
